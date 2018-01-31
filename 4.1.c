805c37911b844a0bb3bca09be6a9233e07e089ea
MaciejKutyla <35468919+MaciejKutyla@users.noreply.github.com>
2018-01-31 19:21:06 +0100

    Add files via upload

--- 4.1.c
+++ 4.1.c
@@ -3,17 +3,21 @@
 #include <stdio.h>
 #include <math.h>
 
+double liczenie_pierwiastkow(double a, double b, double c);
+
 double liczenie_pierwiastkow(double a, double b, double c)
 {
-    double delta, x1, x2;
+    double delta;
     delta=b*b-4*a*c;
     if (delta==0)
     {
+        double x1;
         x1=(-b)/(2*a);
         printf("Mamy jeden pierwiastek rzeczywisty: \nx=%lf", x1);
     }
     else if (delta>0)
     {
+        double x1, x2;
         x1=((-b)-(sqrt(delta)))/(2*a);
         x2=((-b)+(sqrt(delta)))/(2*a);
         printf("Mamy dwa pierwiastki rzeczywiste:\nx1=%lf\nx2=%lf", x1, x2);
@@ -22,16 +26,21 @@ double liczenie_pierwiastkow(double a, double b, double c)
         printf("Rownanie nie ma pierwiastkow rzeczywistych");
 }
 
-
-
 int main(void)
 {
-    double a,b,c;
     printf("Podaj wspolczynniki rownania kwadratowego: \na=");
+    double a;
     scanf("%lf", &a);
+    if (a==0)
+    {
+        printf("Nie jest to rownanie kwadratowe!\n");
+        exit(0);
+    }
     printf("b=");
+    double b;
     scanf("%lf", &b);
     printf("c=");
+    double c;
     scanf("%lf", &c);
     liczenie_pierwiastkow(a, b, c);
 
